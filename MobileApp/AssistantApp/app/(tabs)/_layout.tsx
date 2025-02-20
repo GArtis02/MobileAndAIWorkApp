db02c90f9e702ee4f6b249de50206a2e3f7f0cac
GArtis02 <artis.gailisb@gmail.com>
2025-02-20 22:03:03 +0200

    test

--- MobileApp/AssistantApp/app/(tabs)/_layout.tsx
+++ MobileApp/AssistantApp/app/(tabs)/_layout.tsx
@@ -40,6 +40,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+       <Tabs.Screen
+        name="test"
+        options={{
+          title: 'test',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
+    
   );
 }
